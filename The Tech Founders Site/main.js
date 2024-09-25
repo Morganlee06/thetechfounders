f3a73a34895ff24166d3032ed8572025f1c2b540
Morgan lee <148326353+Morganlee06@users.noreply.github.com>
2024-09-25 17:01:44 +0100

    A fully functional tech blog with newsletter
    Completed Project

---
+++ The Tech Founders Site/main.js
@@ -0,0 +1,25 @@
+const menuBtn = document.querySelector('.menu-btn')
+const navlinks = document.querySelector('.nav-links')
+
+menuBtn.addEventListener('click',()=>{
+    navlinks.classList.toggle('mobile-menu')
+})
+
+const goTopBtn = document.querySelector('.go-top-btn');
+
+window.addEventListener('scroll', checkHeight)
+
+function checkHeight(){
+  if(window.scrollY > 200) {
+    goTopBtn.style.display = "flex"
+  } else {
+    goTopBtn.style.display = "none"
+  }
+}
+
+goTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+})
